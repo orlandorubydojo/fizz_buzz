d42db3651ed10b5b12a8bf8ef72075d8e2494f9a
Nestor Castillo <nestor.castillo@izea.com>
2014-09-17 20:18:30 -0400

    Dojo progress

--- lib/fizz_buzzer.rb
+++ lib/fizz_buzzer.rb
@@ -1,3 +1,24 @@
+require "byebug"
+
 class FizzBuzzer
+
+  def self.buzz_me(number)
+    if number % 15 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
+    elsif number % 5 == 0
+      "Buzz"
+    else
+      number.to_s
+    end
+  end
+
+  def self.print_me(start, finish)
+    a = (start..finish).map do |num|
+      self.buzz_me(num)
+    end
+    a.join "\n"
+  end
 
 end
--- spec/fizz_buzzer_spec.rb
+++ spec/fizz_buzzer_spec.rb
@@ -2,6 +2,43 @@ require './lib/fizz_buzzer'
 
 describe FizzBuzzer do
   it "prints out 1 when 1 is given" do
-
+    expect(FizzBuzzer.buzz_me(1)).to eq "1"
   end
+
+  it "print out 'Fizz' when 3 is given" do
+    expect(FizzBuzzer.buzz_me(3)).to eq "Fizz"
+  end
+
+  it "print out 'Buzz' when 5 is given" do
+    expect(FizzBuzzer.buzz_me(5)).to eq "Buzz"
+  end
+
+  it "print out 'FizzBuzz' when 15 is given" do
+    expect(FizzBuzzer.buzz_me(15)).to eq "FizzBuzz"
+  end
+
+  it "prints out Fizz when 6 is given" do
+    expect(FizzBuzzer.buzz_me(6)).to eq "Fizz"
+  end
+
+  it "prints out 6 when 6 is given" do
+    expect(FizzBuzzer.buzz_me(199)).to eq "199"
+  end
+
+
+  it "should return 1" do
+    expect(FizzBuzzer.print_me(1,1)).to eq "1"
+  end
+
+   it " 1,2 should return 1 newline 2" do
+      expect(FizzBuzzer.print_me(1,2)).to eq "1\n2"
+   end
+
+   it " 1,10 should return 1 newline 2" do
+      expect(FizzBuzzer.print_me(1,10)).to eq "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz"
+   end
+
+   it " 1,15 should return 1 newline 2" do
+      expect(FizzBuzzer.print_me(1,15)).to eq "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz"
+   end
 end
